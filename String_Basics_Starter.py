f9a08bbc8b5e7130fdd88923b1729f12a61f4c2e
Richard Azia <richard@main-vision.com>
2022-07-23 11:32:40 +0200

    my solution

--- String_Basics_Starter.py
+++ String_Basics_Starter.py
@@ -5,18 +5,20 @@ last = "Hulkenberg"
 # - Create a variable called "full_name" that combines first and last with a space between them.  
 # - Print it out
 
-
+full_name = first + " " + last
+print(full_name)
 
 # - Create an "initials" variable that holds the first character of first followed by the first character of last. 
 # - Print it out
 
-
+print(first[0] + " " + last[0])
 
 # - Create an "initials_2" variable that holds the first character of first followed by the first character of last, with periods after each letter!
 # - Print it out
 
-
+print(first[0] + "." + last[0] + ".")
 
 # Create a "nickname" variable that holds the first 4 characters of "last" (use a slice)
 # Print it out 
 
+print(last[0:4])
