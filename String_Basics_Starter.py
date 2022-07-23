1addb3f80552681cf16d28a66d5466a3cfc5bed8
Richard Azia <richard@main-vision.com>
2022-07-23 11:24:59 +0200

    starter file

---
+++ String_Basics_Starter.py
@@ -0,0 +1,22 @@
+
+first = "Nico"
+last = "Hulkenberg"
+
+# - Create a variable called "full_name" that combines first and last with a space between them.  
+# - Print it out
+
+
+
+# - Create an "initials" variable that holds the first character of first followed by the first character of last. 
+# - Print it out
+
+
+
+# - Create an "initials_2" variable that holds the first character of first followed by the first character of last, with periods after each letter!
+# - Print it out
+
+
+
+# Create a "nickname" variable that holds the first 4 characters of "last" (use a slice)
+# Print it out 
+
