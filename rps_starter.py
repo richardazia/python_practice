b6fd49462d05e2d16f1bd798a09734051bca5d26
Richard Azia <richard@main-vision.com>
2022-07-27 10:50:29 +0200

    Logic for rock paper scissors added

--- rps_starter.py
+++ rps_starter.py
@@ -27,17 +27,50 @@ scissors = """
 """
 # Pick a random number from 1 to 3
 num = randint(1,3)
+print(num)
 
 # Turn that random number into the computer's RPS move
 
 
 # Ask a user to enter their move
-
+print("Choose between 1: rock, 2: paper, 3: scissors")
+player = int(input("Enter the number of your selection: "))
 
 # Print the rock, paper, or scissors ASCII art that corresponds to the player's move
-
+print("")
+print("")
+print("Player move: ")
+if player == 1:
+    print(rock)
+elif player == 2:
+    print(paper)
+elif player == 3:
+    print(scissors)
 
 # Print the rock, paper, or scissors ASCII art that corresponds to the computer's move
-
+print("Computer move")
+if num == 1:
+    print(rock)
+elif num == 2:
+    print(paper)
+elif num == 3:
+    print(scissors)
 
 # Figure out who wins and print the result!
+if num == player:
+    print("You both tied!!")
+elif num == 1 and player == 3:
+    print("Computer wins!!")
+elif num == 3 and player == 2:
+    print("Computer wins!!")
+elif num == 2 and player == 1:
+    print("Computer wins!!")
+elif player == 1 and num == 3:
+    print("player wins")
+elif player == 3 and num == 2:
+    print("player wins")
+elif player == 2 and num == 1:
+    print("player wins")
+# - Rock beats scissors
+# - Scissors beats paper
+# - Paper beats rock
