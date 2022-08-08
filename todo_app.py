bcda3c946e15844f64a3ce358308b6ffbd562d52
Richard Azia <richard@main-vision.com>
2022-08-08 10:57:17 +0200

    added functionality thanks to the course video

--- todo_app.py
+++ todo_app.py
@@ -28,22 +28,36 @@ print("*" * 40)
 #####################
 # Course solution
 todos = []
+finished = []
 
 while True:
-    for todo in todos:
-        print(f"* {todo}")
+    # This is where I got stuck when working on my own solution. Make the tasks part of a range. 
+    for i in range(len(todos)):
+        print(f"* {i+1}. {todos[i]}")
     print("*" * 45)
     print("Enter a command. Type 'h' for help:")
     command = input("> ")
-    if command == "q":
+    if command == "q" or command == "quit":
         break
+    elif command == "h" or command == "help":
+        print("*" * 35)
+        print("Command options: ")
+        print("Simply type in your task and press enter.")
+        print("To delete a task just type the task number and enter.")
+        print("To exit the app press 'q' and enter.")
+        print("*" * 35)
+    elif command.isnumeric():
+        i = int(command) -1
+        if i >= len(todos):
+            print(f"{i + 1} does not exist yet! ")
+        else:
+            # remove the completed task by popping it. Take what is returned and add it to the completed list
+            completed_task = todos.pop(i)
+            finished.append(completed_task)
     else: 
         todos.append(command)
 
 print("Tata For Now!")
-
-
-
-
-        
-
+print(f"We completed {len(finished)} tasks today: ")
+for todo in finished:
+    print(f"* {todo}")
