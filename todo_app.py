d16219d9ac570e95834bed6ae5da20584a4ef1b2
Richard Azia <richard@main-vision.com>
2022-08-08 10:19:57 +0200

    first step - get things to display

--- todo_app.py
+++ todo_app.py
@@ -10,27 +10,40 @@
 print("*" * 40)
 print("The Todo App")
 print("*" * 40)
-i = 0
-todo = []
-todo_tasks = []
+# i = 0
+# todo = []
+# todo_tasks = []
 
-print("To add a ToDo simply type the todo and press enter.")
-print("To mark a ToDo as done type the item's relevant number.")
-while todo != "q":
-    todo = input("Please enter your new ToDo: ")
-    todo_tasks.append(todo)
-    print(todo)
-    print(todo_tasks)
-    for todo_task in todo_tasks:
-        i+=1
-        print(f"{i}. {todo_task}")
+# print("To add a ToDo simply type the todo and press enter.")
+# print("To mark a ToDo as done type the item's relevant number.")
+# while todo != "q":
+#     todo = input("Please enter your new ToDo: ")
+#     todo_tasks.append(todo)
+#     print(todo)
+#     print(todo_tasks)
+#     for todo_task in todo_tasks:
+#         i+=1
+#         print(f"{i}. {todo_task}")
+
+#####################
+# Course solution
+todos = []
+
+while True:
+    for todo in todos:
+        print(f"* {todo}")
+    print("*" * 45)
+    print("Enter a command. Type 'h' for help:")
+    command = input("> ")
+    if command == "q":
+        break
+    else: 
+        todos.append(command)
+
+print("Tata For Now!")
+
+
+
+
         
 
-
-        
-
-
-# for meeting in meetings:
-#     print(meeting)
-#     for specific in meeting:
-#         print(f"Room booked for {specific}. Please check e-mail for confirmation")
