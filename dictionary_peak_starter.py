2803c2a30d4b25297373462914b9157199cca59b
Richard Azia <richard@main-vision.com>
2022-08-10 11:12:34 +0200

    course corrections

--- dictionary_peak_starter.py
+++ dictionary_peak_starter.py
@@ -13,18 +13,21 @@ peak = {
 peak['range'] = "Elk Mountains"
 
 # Add a "first_climbed" key to peak and set it equal to 1873
-peak['first_climbed'] = "1873"
+peak['first_climbed'] = 1873
 
 # Whoops, there's a mistake with the peak "height".  Update it to 14265
 peak["height"] = 14625
 
 # Add a "Verizon" to the "cell_reception" dict and set it equal to "good"
 # I do not know how to do this one. 
-verizon = {"verizon" : "good"}
-peak["cell_reception"][2] = verizon
+# verizon = {"verizon" : "good"}
+# peak["cell_reception"][2] = verizon
+#### Correction
+peak["cell_reception"]["Verizon"] = "good"
 
 # You just summited the peak! Add your name to the "summit_log" list
-peak["summit_log"] = "Richard A."
+# peak["summit_log"] = "Richard A."
+peak["summit_log"].append("Richard")
 
 # Let's rename "height" to "elevation":
 
@@ -32,11 +35,14 @@ peak["summit_log"] = "Richard A."
 height = peak.pop("height")
 
 # Use the value for "height" and store it in the dict under they key "elevation"
-elevation = height
-print(elevation)
+peak["elevation"] = height
 # Loop over the values in the dictionary and print them all out.  Don't ask why, just do it :) 
-print("Values")
+print("\n Values")
 print(peak.values())
+# Course alternative:
+print("\n Course version")
+for val in peak.values():
+    print(val)
 
 # Loop over the keys AND values in the dictionary and print them all out in the following format:
 # key -> value
@@ -49,5 +55,5 @@ for key, value in peak.items():
 
 # A huge earthquake/meteor/forestfire/tsunami has destroyed the peak.  Please empty out the entire dictionary.
 peak.clear()
-
+print("\n\n Data cleared from dictionary")
 print(peak)
