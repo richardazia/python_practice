416b3bf7bb8e2a7ebe6c1ece980b4c2fd876a93d
Richard A <richard@main-vision.com>
2022-08-19 10:14:15 +0200

    add directory

---
+++ python3/python3.py
@@ -0,0 +1,29 @@
+# COLT Steele Python 3 course notes
+
+# We can use += with strings
+
+#month = "The month of"
+#month += " August, and it is the 18th day"
+
+#print(month)
+
+#print("How far did you cycle today?")
+#distance = input()
+#distance_in_miles = float(distance) * 0.621371
+#print(f"You cycled {distance} kilometres. That is equal to {round(distance_in_miles, 2)} miles.")
+
+#print("How far did you cycle this year?")
+#distance = input()
+#distance_in_miles = float(distance) * 0.621371
+#print(f"You cycled {distance} kilometres. That is equal to {round(distance_in_miles, 2)} miles.")
+
+# NO TOUCHING PLEASE---------------
+from random import randint
+choice = randint(1,10)
+# NO TOUCHING PLEASE---------------
+
+# YOUR CODE GOES HERE:
+if choice == 7:
+    print("lucky")
+elif choice != 7:
+    print("unlucky")
