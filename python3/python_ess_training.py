ecab2e370679a2b236cf685604c1c2e3078e40bf
Richard A <richard@main-vision.com>
2022-08-28 12:04:06 +0200

    experiment with termcolor

---
+++ python3/python_ess_training.py
@@ -0,0 +1,17 @@
+import sys
+from termcolor import colored, cprint
+
+text = colored('Hello, World!', 'red', attrs=['reverse', 'blink'])
+print(text)
+cprint('Hello, World!', 'green', 'on_red')
+
+print_green_on_cyan = lambda x: cprint(x, 'green', 'on_cyan')
+
+print_green_on_cyan("The Very Quick Brown Fox")
+
+
+for i in range(21):
+    cprint(i, 'blue', end=' ')
+
+cprint("Attention!", 'red', attrs=['bold'], file=sys.stderr)
+cprint("Warning!", 'yellow', attrs=['bold'], file=sys.stderr)
