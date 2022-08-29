0cc43994388b27445ef4a6dabf30a7bd700bf73c
Richard A <richard@main-vision.com>
2022-08-29 09:14:30 +0200

    experiment with class and smog for cough and eyes

--- python3/python_ess_training.py
+++ python3/python_ess_training.py
@@ -17,3 +17,32 @@ cprint("Attention!", 'red', attrs=['bold'], file=sys.stderr)
 cprint("Warning!", 'yellow', attrs=['bold'], file=sys.stderr)
 
 # see: https://pypi.org/project/termcolor/
+
+def multiplication(v1, v2):
+  return v1 * v2
+
+print(print(multiplication(12,6)))
+
+class Smog:
+  def __init__(self, name):
+    self.name = name
+    self.weather = 'True'
+    self.pollution = "combustion sources"
+
+  def cough(self):
+    print("Cough Cough" + " " + self.name + ", behave. ")
+
+  def eyes(self):
+    print('My eyes are stinging')
+
+my_smog = Smog("Flora")
+not_good = Smog("Not Good, at all")
+
+print("The Smog function")
+my_smog.eyes()
+my_smog.cough()
+not_good.cough()
+not_good.eyes()
+
+
+
