59433dac02bf79279c7a0ca148988c85dac0de7d
Richard A <richard@main-vision.com>
2022-09-02 14:47:36 +0200

    web scraping experimentation via the web scraping course on Linkedin

---
+++ web_scraping/ietf_scraper/ietf_scraper/spiders/ietf.py
@@ -0,0 +1,14 @@
+import scrapy
+
+
+class IetfSpider(scrapy.Spider):
+    name = 'ietf'
+    allowed_domains = ['pythonscraping.com']
+    start_urls = ['http://pythonscraping.com/linkedin/ietf.html']
+
+    def parse(self, response):
+        # title = response.css('span.title::text').get()
+        title = response.xpath('//span[@class="title"]/text()').get()
+        text = response.xpath('//span[@class="text"]/text()').get()
+
+        return [{"title": title}, {"text": text}]
