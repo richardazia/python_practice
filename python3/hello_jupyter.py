ddaa89849bd2f31509c5d3416c700ed25721c93b
Richard A <richard@main-vision.com>
2022-09-06 10:57:27 +0200

    experimenting with jupyter books

---
+++ python3/hello_jupyter.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello world")
+
+
+# In[2]:
+
+
+phrase = "The Quick brown fox looked around carefully"
+
+
+
+# In[4]:
+
+
+print(phrase)
+
+
+# In[5]:
+
+
+len(phrase)
+
+
+# In[6]:
+
+
+type(phrase)
+
+
+# In[7]:
+
+
+type(type)
+
+
+# In[8]:
+
+
+greeting = "Good morning, enjoying the rain?"
+
+
+# In[9]:
+
+
+greeting
+
+
+# In[11]:
+
+
+name = input("What is your name? ")
+
+
+# In[12]:
+
+
+print(f"{greeting} {name}, will you walk in the rain? ")
+
+
+# In[ ]:
+
+
+
+
