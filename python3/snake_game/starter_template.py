4baee28d43ed9b6251abb28d0c9b6cd24b849647
Richard A <richard@main-vision.com>
2022-09-10 10:44:35 +0200

    Give the turtle a box to play in

---
+++ python3/snake_game/starter_template.py
@@ -0,0 +1,23 @@
+import turtle
+
+# The constants
+WIDTH = 500
+HEIGHT = 500
+
+# create the canvas/window
+screen = turtle.Screen()
+screen.setup(WIDTH, HEIGHT)
+screen.title("The Garden")
+screen.bgcolor("cyan")
+
+# And now concolidation, genesis the Turtle, named after my bike, not the band
+
+# The Turtle
+genesis_the_turtle = turtle.Turtle()
+genesis_the_turtle.shape("turtle")
+genesis_the_turtle.color("green")
+
+# Entice the turtle to move around
+
+genesis_the_turtle.forward(100)
+
