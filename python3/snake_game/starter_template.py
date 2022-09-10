065e73b3df243e1a9d6df26bbf173180f26114ee
Richard A <richard@main-vision.com>
2022-09-10 10:55:15 +0200

    Make the turtle go around in circles on a loop

--- python3/snake_game/starter_template.py
+++ python3/snake_game/starter_template.py
@@ -3,12 +3,22 @@ import turtle
 # The constants
 WIDTH = 500
 HEIGHT = 500
+DELAY = 20
+
+# Tell the turtle what to do
+
+def move_turtle():
+    genesis_the_turtle.forward(1)
+    genesis_the_turtle.right(1)
+    screen.update()
+    screen.ontimer(move_turtle, DELAY)
 
 # create the canvas/window
 screen = turtle.Screen()
 screen.setup(WIDTH, HEIGHT)
 screen.title("The Garden")
 screen.bgcolor("cyan")
+screen.tracer(0) # We can also use False. It turns off the trace behind the turtle
 
 # And now concolidation, genesis the Turtle, named after my bike, not the band
 
@@ -19,5 +29,6 @@ genesis_the_turtle.color("green")
 
 # Entice the turtle to move around
 
-genesis_the_turtle.forward(100)
+move_turtle()
 
+turtle.done()
