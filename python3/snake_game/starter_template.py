ac2120b825592903f7a7935c4561d4ed8e29d1b6
Richard A <richard@main-vision.com>
2022-09-10 11:33:52 +0200

    stamp twice

--- python3/snake_game/starter_template.py
+++ python3/snake_game/starter_template.py
@@ -18,14 +18,20 @@ screen = turtle.Screen()
 screen.setup(WIDTH, HEIGHT)
 screen.title("The Garden")
 screen.bgcolor("cyan")
-screen.tracer(0) # We can also use False. It turns off the trace behind the turtle
+screen.tracer(0)  # We can also use False. It turns off the trace behind the turtle
 
 # And now concolidation, genesis the Turtle, named after my bike, not the band
 
 # The Turtle
 genesis_the_turtle = turtle.Turtle()
-genesis_the_turtle.shape("turtle")
+genesis_the_turtle.shape("circle")
 genesis_the_turtle.color("green")
+genesis_the_turtle.shapesize(60 / 20)  # where 60 is the desired size and 20 is the default value.
+genesis_the_turtle.stamp()
+genesis_the_turtle.penup()
+genesis_the_turtle.shapesize(10 / 20)
+genesis_the_turtle.goto(120, 240)
+genesis_the_turtle.stamp()
 
 # Entice the turtle to move around
 
