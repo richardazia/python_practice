860036d0c261421e352b59babfd4c9302deebfdd
Richard A <richard@main-vision.com>
2022-09-10 10:43:58 +0200

    first play with the turtle

---
+++ python3/snake_game/oop_turtle.py
@@ -0,0 +1,14 @@
+import turtle
+
+# The Turtle
+genesis_the_turtle = turtle.Turtle()
+genesis_the_turtle.shape("turtle")
+genesis_the_turtle.color("green")
+
+# Entice the turtle to move around
+
+genesis_the_turtle.forward(100)
+
+# With turtle we use done to run code.
+
+genesis_the_turtle.done()
